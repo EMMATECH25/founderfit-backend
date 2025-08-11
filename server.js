69a272ea226162cd085a5e0a4efe32982d552772
NwogeEmmanuel <nwogeemmanuel917@gmail.com>
2025-08-12 00:18:14 +0100

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const userRoutes = require('./routes/user.routes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001', // for local frontend testing
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
 
 // Routes
