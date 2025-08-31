448fb28cd7b8b5557a5624b5031bf54231d35b2c
NwogeEmmanuel <nwogeemmanuel917@gmail.com>
2025-08-31 20:28:32 +0100

    solved the auth.middleware issue

--- routes/day3.routes.js
+++ routes/day3.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { saveDay3, getDay3 } = require("../controllers/day3.controller");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/auth.middleware");
 
 router.post("/day3/save", auth, saveDay3);
 router.get("/day3/get", auth, getDay3);
